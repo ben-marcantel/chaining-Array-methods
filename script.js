22acb09ddcdee33769ada01177e239cb20574899
MyMac <mymac@mymacs-mbp.attlocal.net>
2017-12-09 10:25:18 -0600

    finished

---
+++ script.js
@@ -0,0 +1,9 @@
+var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
+
+
+console.log(integers.sort(function (a,b){return a-b}).filter(nums=> nums<19).map(nums=>(nums * 1.5)-1).reduce((a,b) => a+b))
+
+
+        
+
+    
